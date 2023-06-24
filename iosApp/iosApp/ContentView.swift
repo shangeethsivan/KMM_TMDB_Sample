5885b95ba6b1d74b653855a2b6356b22f8b752ce
Shangeeth Sivan <shangeeth95@gmail.com>
2023-06-24 21:31:50 +0530

    Added list view in swift and updated iphone os target to get support for AsyncImage

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -6,24 +6,49 @@ struct ContentView: View {
     
     @StateViewModel var viewModel = MainScreenViewModel()
     
-	let greet = Greeting().greet()
-
-	var body: some View {
+    let greet = Greeting().greet()
+    
+    var body: some View {
         let uistate = viewModel.uiState.value
         switch uistate{
-            case is MainScreenViewModelUiStateLoading: Text("Loading");
+        case is MainScreenViewModelUiStateLoading: Text("Loading");
         case let uiState2 as MainScreenViewModelUiStateData :
-            let moviesString = uiState2.movies.map({$0.title}).joined(separator : ",")
-            Text(moviesString)
+            List{
+                ForEach(uiState2.movies, id: \.self){ movie in
+                    HStack{
+                        AsyncImage(
+                            url: URL(string: "\(TMDBApi.companion.IMAGE_BASE_URL)\(movie.imagePath)"),
+                            content: { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView().fixedSize()
+                                case .success(let image):
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: 100, maxHeight: 200)
+                                case .failure:
+                                    Image(systemName: "Load failed")
+                                default:
+                                    Image(systemName: "Load failed")
+                                }
+                            }
+                        )
+                        VStack(alignment: .leading){
+                            Text("**\(movie.title)**")
+                            Text(movie.description_)
+                        }
+                    }
+                    
+                }
+            }
         case is MainScreenViewModelUiStateError: Text("Error");
         default : Text("Unknown");
         }
-        Text(greet)
-	}
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
