0134799a2897d339d36713c5f4bf6a1d1164f8a0
Shangeeth Sivan <shangeeth95@gmail.com>
2023-06-24 20:47:11 +0530

    Added swift test code.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,11 +1,24 @@
 import SwiftUI
 import shared
+import KMMViewModelSwiftUI
 
 struct ContentView: View {
+    
+    @StateViewModel var viewModel = MainScreenViewModel()
+    
 	let greet = Greeting().greet()
 
 	var body: some View {
-		Text(greet)
+        let uistate = viewModel.uiState.value
+        switch uistate{
+            case is MainScreenViewModelUiStateLoading: Text("Loading");
+        case let uiState2 as MainScreenViewModelUiStateData :
+            let moviesString = uiState2.movies.map({$0.title}).joined(separator : ",")
+            Text(moviesString)
+        case is MainScreenViewModelUiStateError: Text("Error");
+        default : Text("Unknown");
+        }
+        Text(greet)
 	}
 }
 
---
+++ iosApp/iosApp/KMMViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  KMMViewModel.swift
+//  iosApp
+//
+//  Created by Shangeeth Sivan on 24/06/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import KMMViewModelCore
+import shared // This should be your shared KMM module
+
+extension Kmm_viewmodel_coreKMMViewModel: KMMViewModel { }
